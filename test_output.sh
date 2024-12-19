13bfd57ade89f10c606b0a69db69fe2aad35ded6
troelsy <troels@ynddal.dk>
2024-12-19 09:17:16 +0000

    Added test of .bin output

---
+++ test_output.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+w_out_md5=$(md5sum "$1/Wout.bin" | cut -d ' ' -f 1)
+w_test_md5=$(md5sum "$1/Wtest.bin" | cut -d ' ' -f 1)
+h_out_md5=$(md5sum "$1/Hout.bin" | cut -d ' ' -f 1)
+h_test_md5=$(md5sum "$1/Htest.bin" | cut -d ' ' -f 1)
+
+if [ "$w_out_md5" != "$w_test_md5" ]; then
+    echo "Wout.bin and Wtest.bin are different"
+    exit 1
+fi
+
+if [ "$h_out_md5" != "$h_test_md5" ]; then
+    echo "Hout.bin and Htest.bin are different"
+    exit 1
+fi
