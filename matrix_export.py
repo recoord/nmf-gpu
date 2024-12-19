dfc10d03c3fff43a47cb08746c4de320e7dd64f1
troelsy <troels@ynddal.dk>
2024-12-19 09:02:39 +0000

    Lock random seed for test data

--- matrix_export.py
+++ matrix_export.py
@@ -1,18 +1,10 @@
 import numpy as np
 import struct
 
-# # target X matrix
-# X = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=np.float32)
-# # initial W matrix
-# W = np.array([[1, 2], [3, 4], [5, 6], [7, 8]], dtype=np.float32)
-# # initial H matrix
-# H = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float32)
-
+np.random.seed(0)
 X = np.random.rand(4096, 3500).astype(np.float32)
 W = np.random.rand(4096, 128).astype(np.float32)
 H = np.random.rand(128, 3500).astype(np.float32)
-
-path = "./"
 
 def write_matrix_to_file(matrix, filename):
     with open(filename, "wb") as f:
@@ -20,6 +12,6 @@ def write_matrix_to_file(matrix, filename):
         f.write(matrix.tobytes())
         print(f"wrote file with {matrix.size} elements")
 
-write_matrix_to_file(X, f"{path}X.bin")
-write_matrix_to_file(W, f"{path}W.bin")
-write_matrix_to_file(H, f"{path}H.bin")
+write_matrix_to_file(X, "X.bin")
+write_matrix_to_file(W, "W.bin")
+write_matrix_to_file(H, "H.bin")
